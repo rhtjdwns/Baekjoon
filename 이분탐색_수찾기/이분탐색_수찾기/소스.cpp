a414ee091da0f94ae152ea0d1616049ef6c552d2
rhtjdwns <rhtjdwns@gmail.com>
2022-06-28 00:49:55 +0900

    주석추가

--- 이분탐색_수찾기/이분탐색_수찾기/소스.cpp
+++ 이분탐색_수찾기/이분탐색_수찾기/소스.cpp
@@ -9,16 +9,20 @@ void Search(int* a,int b, int n)
 	int	start = 0;
 	int end = n;
 
+
+	// 시작점과 끝점이 만나지 않았다면 반복
 	while (start < end)
 	{
 		mid = (start + end) / 2;
 		if (b == a[mid])
 		{
+			// 찾으려는 수가 존재한다면
 			cout << "1" << endl;
 			return;
 		}
 		else
 		{
+			// 시작값 또는 끝값을 현재 값과 비교하여 위치를 조정
 			if (b > a[mid])
 			{
 				start = mid + 1;
@@ -29,6 +33,7 @@ void Search(int* a,int b, int n)
 			}
 		}
 	}
+	// 찾으려는 수가 존재하지 않는다면
 	cout << "0" << endl;
 	return;
 }
@@ -46,6 +51,7 @@ int main()
 	}
 	cin >> m;
 
+	// 오름차순 정렬
 	sort(a, a + n);
 	for (int i = 0; i < m; ++i)
 	{
