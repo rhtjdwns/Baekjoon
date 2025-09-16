91aa1f0b0a5bcceb8de20ac72b0344996299212a
rhtjdwns <64015904+rhtjdwns@users.noreply.github.com>
2025-09-16 20:17:11 +0900

    [Silver II] Title: 최소 힙, Time: 64 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+
+int main()
+{
+	priority_queue<int, vector<int>, greater<int>> pQueue;
+	queue<int> results;
+	int N, M;
+
+	cin >> N;
+
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> M;
+		if (M == 0)
+		{
+			if (pQueue.size() == 0)
+			{
+				results.push(0);
+			}
+			else
+			{
+				results.push(pQueue.top());
+				pQueue.pop();
+			}
+		}
+		else
+		{
+			pQueue.push(M);
+		}
+	}
+
+	int size = results.size();
+	for (int i = 0; i < size; ++i)
+	{
+		cout << results.front() << "\n";
+		results.pop();
+	}
+}
